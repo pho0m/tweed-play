91b72276da3564960a87d3477cb0b2d1580adc2c
pho0m <pho0m2000s@gmail.com>
2021-09-16 21:24:19 +0700

    add log

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const {
 	token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+    client.login(token);
+
+    client.once('ready', () => {
+        console.log('Ready!');
+   });
+
+    client.once('reconnecting', () => {
+        console.log('Reconnecting!');
+   });
+
+    client.once('disconnect', () => {
+        console.log('Disconnect!');
+   });
+
